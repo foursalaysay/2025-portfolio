d673ad58a4f3386547c9bafc496776e27ca9cffe
John Kyle Salaysay <foursalaysay@gmail.com>
2024-12-31 01:21:44 +0800

    first commit

---
+++ src/components/component/LeftProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function LeftProfile() {
+  return (
+    <div className='flex flex-col items-center gap-3 pl-10'>
+        <h5 className='text-gray-400'>@0x_kael</h5>
+        <h4 className='text-white font-bold text-2xl'>John Kyle Salaysay</h4>
+        <h6>* 09165352775 *</h6>
+        <h6>* foursalaysay@gmail.com *</h6>
+    </div>
+  )
+}
---
+++ src/components/component/RightInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RightInfo() {
+  return (
+    <div className='w-full h-full pl-10 pt-20'>
+        
+    </div>
+  )
+}
